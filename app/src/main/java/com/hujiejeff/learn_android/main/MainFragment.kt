7852e5ddd631de7f541d0199a11ee33e9827a46f
hujiejeff <hujiejeff@163.com>
2023-01-29 15:18:25 +0800

    dialog代码

--- app/src/main/java/com/hujiejeff/learn_android/main/MainFragment.kt
+++ app/src/main/java/com/hujiejeff/learn_android/main/MainFragment.kt
@@ -1,16 +1,21 @@
 package com.hujiejeff.learn_android.main
 
+import android.app.AlertDialog
+import android.graphics.drawable.BitmapDrawable
+import android.view.LayoutInflater
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.activityViewModels
 import com.hujiejeff.learn_android.base.BaseFragment
-import com.hujiejeff.learn_android.materia_design.constraintlayout.ConstrainLayoutHomeDemoActivity
+import com.hujiejeff.learn_android.databinding.DialogSampleBinding
 import com.hujiejeff.learn_android.databinding.FragmentMainBinding
 import com.hujiejeff.learn_android.jetpack.vm.LoginActivity
 import com.hujiejeff.learn_android.jetpack.vm.LoginViewModel
+import com.hujiejeff.learn_android.materia_design.constraintlayout.ConstrainLayoutHomeDemoActivity
 import com.hujiejeff.learn_android.materia_design.toolbar.ToolbarDemoFragment
 import com.hujiejeff.learn_android.util.clickJump
 import com.hujiejeff.learn_android.util.newInstance
 import com.hujiejeff.learn_android.util.startFragmentByShareAnimator
+
 
 class MainFragment : BaseFragment<FragmentMainBinding>() {
     val viewModel: LoginViewModel by activityViewModels()
@@ -27,5 +32,15 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
             )
         }
         btnMvvmDemo.clickJump<LoginActivity>()
+        btnOpenAlertDialog.setOnClickListener {
+            val dialogBinding = DialogSampleBinding.inflate(LayoutInflater.from(context))
+            val dialog =
+                AlertDialog.Builder(context)
+                    .setView(dialogBinding.root)
+                    .setCancelable(true)
+                    .create()
+            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent) //消除白块
+            dialog.show()
+        }
     }
 }
