bc2fdc0b6f23ad5519d450521e4879448f21840c
hujiejeff <hujiejeff@163.com>
2023-09-07 17:39:04 +0800

    添加测试gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.typeconversion.TimeUnitsParser
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.0.2" apply false
@@ -7,12 +9,27 @@ plugins {
 }
 
 //classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-tasks.register("welcome"){
+tasks.register("welcome") {
     doLast {
         println("Welcome to Gradle")
     }
 }
 
-tasks.register("clean"){
+tasks.register("clean") {
+    dependsOn("welcome")
     delete(rootProject.buildDir)
+}
+
+tasks.register("my_install") {
+    var startTime = 0L
+    var endTime = 0L
+    println("Start my_install ====>")
+    startTime = System.currentTimeMillis()
+    dependsOn("app:assembleDebug")
+    doLast {
+        endTime = System.currentTimeMillis()
+        val str = ((endTime - startTime) / 1000)
+        println("End my_install <====")
+        println("use time ${endTime - startTime}ms")
+    }
 }
