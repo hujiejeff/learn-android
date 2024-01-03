06d1f83a8f09e54fb24b2e53c75d33065c984374
hujiejeff <hujiejeff@163.com>
2024-01-03 15:41:12 +0800

    savedStateHandle使用

--- app/src/main/java/com/hujiejeff/learn_android/jetpack/vm/LoginViewModel3.kt
+++ app/src/main/java/com/hujiejeff/learn_android/jetpack/vm/LoginViewModel3.kt
@@ -5,7 +5,14 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel3(val savedStateHandle: SavedStateHandle): ViewModel() {
+    val key = "test_key"
+    init {
+        savedStateHandle[key] = "init"
+    }
     fun hello() {
         Log.d("LoginViewModel3" , savedStateHandle.toString())
+        Log.d("LoginViewModel3" , savedStateHandle[key] ?: "empty")
+        savedStateHandle[key] = "hello"
+        Log.d("LoginViewModel3" , savedStateHandle[key] ?: "empty")
     }
 }
