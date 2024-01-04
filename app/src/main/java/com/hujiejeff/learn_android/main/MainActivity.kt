56f72740d6f3e505c163f99d58682672fa794b46
hujiejeff <hujiejeff@163.com>
2024-01-04 14:19:11 +0800

    添加splashscreen闪屏库使用

--- app/src/main/java/com/hujiejeff/learn_android/main/MainActivity.kt
+++ app/src/main/java/com/hujiejeff/learn_android/main/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.hujiejeff.learn_android.main
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.window.layout.WindowInfoTracker
+import com.blankj.utilcode.util.ToastUtils
 import com.hujiejeff.learn_android.R
 import com.hujiejeff.learn_android.base.BaseActivity
 import com.hujiejeff.learn_android.databinding.ActivityMainBinding
@@ -20,6 +29,8 @@ import kotlinx.coroutines.launch
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+    val viewModel: MainViewModel by viewModels()
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun ActivityMainBinding.initView() {
         supportFragmentManager.beginTransaction().replace(R.id.container, newInstance<MainFragment>()).commit()
         lifecycleScope.launch(Dispatchers.Main) {
@@ -33,6 +44,60 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
                     }
             }
         }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //设置splash
+        val sppSplash = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        //设置额外等待时间
+        viewModel.initLoading()
+        ToastUtils.showShort("正在初始化")
+        val container = findViewById<View>(R.id.container)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+
+        //第一种方式使用当前的splash
+        /*container.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // Check whether the initial data is ready.
+                    return if (viewModel.isReady) {
+                        // The content is ready. Start drawing.
+                        container.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        // The content isn't ready. Suspend.
+                        false
+                    }
+                }
+            }
+        )*/
+
+        //使用app的splash
+        sppSplash.setKeepOnScreenCondition {
+            !viewModel.isReady
+        }
+
+
+        //设置离开动画
+        sppSplash.setOnExitAnimationListener { splashScreenViewProvider ->
+            // Create your custom animation.
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenViewProvider.view,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenViewProvider.view.height.toFloat()
+            )
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = 200L
+
+            // Call SplashScreenView.remove at the end of your custom animation.
+            slideUp.doOnEnd { splashScreenViewProvider.remove() }
+            // Run your animation.
+            slideUp.start()
+        }
+
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
--- app/src/main/java/com/hujiejeff/learn_android/main/MainViewModel.kt
+++ app/src/main/java/com/hujiejeff/learn_android/main/MainViewModel.kt
@@ -3,10 +3,19 @@ package com.hujiejeff.learn_android.main
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+     var isReady = false
+    init {
 
-    constructor(application: Application, savedStateHandle: SavedStateHandle) : this(application) {
     }
-
+    fun initLoading() {
+        viewModelScope.launch {
+            delay(2000)
+            isReady = true
+        }
+    }
 }
